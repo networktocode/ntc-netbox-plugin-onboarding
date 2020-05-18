89a78a3e68b87761431c0fabc547cb9544d5ae36
Damien Garros <dgarros@gmail.com>
2020-05-18 17:42:40 -0400

    Increase version to 0.9.2

--- netbox_onboarding/__init__.py
+++ netbox_onboarding/__init__.py
@@ -12,7 +12,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-__version__ = "0.9.1"
+__version__ = "0.9.2"
 
 from extras.plugins import PluginConfig
 
