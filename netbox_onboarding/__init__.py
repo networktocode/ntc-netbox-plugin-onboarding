5d67342b6a35a7fc30cc1a27ac7a295b7d646333
Josh VanDeraa <jv@networktocode.com>
2021-12-06 18:27:46 -0600

    Appeasing NetBox decision to deprecate Py3.6

--- netbox_onboarding/__init__.py
+++ netbox_onboarding/__init__.py
@@ -12,7 +12,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-__version__ = "2.3.0"
+__version__ = "3.0.0"
 
 from extras.plugins import PluginConfig
 
